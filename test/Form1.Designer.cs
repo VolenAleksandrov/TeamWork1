bdcaea6126ca338bd01c50bf4adc74c0dcec152d
vo13nn <vo13nn@gmail.com>
2015-10-04 12:22:38 +0300

    kgbjnlm

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.grounPannel = new System.Windows.Forms.Panel();
+            this.hero = new System.Windows.Forms.PictureBox();
+            this.heroMovement = new System.Windows.Forms.Timer(this.components);
+            this.rockGenerator = new System.Windows.Forms.Timer(this.components);
+            this.gamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.gamePanel.Controls.Add(this.hero);
+            this.gamePanel.Location = new System.Drawing.Point(12, 14);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(400, 440);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // grounPannel
+            // 
+            this.grounPannel.BackColor = System.Drawing.Color.Green;
+            this.grounPannel.Location = new System.Drawing.Point(12, 454);
+            this.grounPannel.Name = "grounPannel";
+            this.grounPannel.Size = new System.Drawing.Size(400, 18);
+            this.grounPannel.TabIndex = 1;
+            // 
+            // hero
+            // 
+            this.hero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.hero.Location = new System.Drawing.Point(61, 390);
+            this.hero.Name = "hero";
+            this.hero.Size = new System.Drawing.Size(30, 50);
+            this.hero.TabIndex = 0;
+            this.hero.TabStop = false;
+            // 
+            // heroMovement
+            // 
+            this.heroMovement.Enabled = true;
+            this.heroMovement.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // rockGenerator
+            // 
+            this.rockGenerator.Enabled = true;
+            this.rockGenerator.Tick += new System.EventHandler(this.rockGenerator_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 476);
+            this.Controls.Add(this.grounPannel);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.gamePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.hero)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.PictureBox hero;
+        private System.Windows.Forms.Timer heroMovement;
+        private System.Windows.Forms.Panel grounPannel;
+        private System.Windows.Forms.Timer rockGenerator;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        int indexRocks = 0;
+        Random randomLocation = new Random();
+        Random randomSize = new Random();
+        Random randomInterval = new Random();
+        PictureBox rock = new PictureBox();
+        List<PictureBox> rocks = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if (hero.Left < 370)
+                    {
+                        hero.Left += 5;
+                    }
+                    break;
+                case Keys.Left:
+                    if (hero.Left > 0)
+                    {
+                        hero.Left -= 5;
+                    }
+                    break;
+                case Keys.Escape:
+                    Application.Exit();
+                    break;
+
+            }
+        }
+
+        private void rockGenerator_Tick(object sender, EventArgs e)
+        {
+            rock = new PictureBox();
+            rock.Left = randomLocation.Next(0, 400);
+            rock.Height = 30;
+            rock.Width = 30;
+        }
+    }
+}
